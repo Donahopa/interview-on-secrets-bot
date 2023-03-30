a066f4a3dbf3ac14d262d1b083e7a2d117829043
Donahopa <128167651+Donahopa@users.noreply.github.com>
2023-03-30 13:30:09 -0400

    Update output.tf

--- terrafom/output.tf
+++ terrafom/output.tf
@@ -3,9 +3,6 @@ output "aws_secretsmanager_secret" {
  
 }
 
-#data "aws_iam_access_key" "interview_bot" {
-#  user_name = aws_iam_user.interview_bot.name
-#}
 
 output "user_id" {
   value = aws_iam_access_key.interview-bot.id
@@ -17,7 +14,3 @@ output "user_secret" {
   value     = nonsensitive(aws_iam_access_key.interview-bot.secret)
 }
 
-# resource "local_file" "interview_bot_credentials" {
-#   content  = "Access key ID: ${aws_iam_access_key.interview-bot.id}\n  Secret access key: ${aws_iam_access_key.interview-bot.secret}\n"
-#   filename = "interview_bot_credentials.txt"
-# }
